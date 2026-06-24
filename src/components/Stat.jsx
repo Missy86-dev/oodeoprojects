e08af8b02776f115d71bbdf240f5b9fb4c010bd6
Missy86-dev <elliot.lola@gmail.com>
2026-06-24 12:32:16 +0100

    Update stat.jsx and stat.css  stat responsiveness for tablet and mobile and remove footer gap

--- src/components/Stat.jsx
+++ src/components/Stat.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import "./stat.css"
+import React from "react";
+import "./stat.css";
 
 const Stat = () => {
-    const statsData = [
+  const statsData = [
     { number: 22, label: "PRODUCT TRAINING" },
     { number: 85, label: "INSTALLATIONS" },
     { number: 75, label: "MAINTENANCE" },
     { number: 15, label: "COUNTRIES" },
   ];
+
   return (
-    <>
-<section className="stats">
+    <section className="stats">
+      <h2 className="stat-title">Projects</h2>
+
       <div className="stats-container">
-        <h2 className="stat-title">Projects</h2>
         {statsData.map((item, index) => (
           <div className="stat-box" key={index}>
             <h1 className="stat-number">{item.number}</h1>
@@ -21,8 +22,40 @@ const Stat = () => {
         ))}
       </div>
     </section>
-    </>
-  )
-}
+  );
+};
 
-export default Stat
+export default Stat;
+
+
+
+
+
+// import React from 'react'
+// import "./stat.css"
+
+// const Stat = () => {
+//     const statsData = [
+//     { number: 22, label: "PRODUCT TRAINING" },
+//     { number: 85, label: "INSTALLATIONS" },
+//     { number: 75, label: "MAINTENANCE" },
+//     { number: 15, label: "COUNTRIES" },
+//   ];
+//   return (
+//     <>
+// <section className="stats">
+//       <div className="stats-container">
+//         <h2 className="stat-title">Projects</h2>
+//         {statsData.map((item, index) => (
+//           <div className="stat-box" key={index}>
+//             <h1 className="stat-number">{item.number}</h1>
+//             <p className="stat-label">{item.label}</p>
+//           </div>
+//         ))}
+//       </div>
+//     </section>
+//     </>
+//   )
+// }
+
+// export default Stat
